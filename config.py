7ba6209b3ad7edc15784ae5955e75cef1dcd3473
PhamHong03 <phamthicamhong0301@gmail.com>
2025-03-03 09:40:24 +0700

    update url send api

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/healthy_and_diagnosis"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/healthy_and_diagnosis"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False

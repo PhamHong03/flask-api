d558043bc11a7f8f274ee4efbfa82fb017ee95e0
PhamHong03 <phamthicamhong0301@gmail.com>
2025-03-04 22:35:32 +0700

    update api from android app

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+
+
 class Config:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/healthy_and_diagnosis"
 
--- routes/user_route.py
+++ routes/user_route.py
@@ -2,8 +2,53 @@
 
 from flask import Blueprint, request, jsonify
 from controllers.user_controller import UserController
+from models.user import User
+from database import db
+import jwt
+
 
 user_bp = Blueprint('user_bp', __name__)
+
+@user_bp.route('/register', methods=['POST'])
+def register_user():
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+
+        if not username or not email or not password:
+            return jsonify({"message": "Không được bỏ trống"}), 400
+        
+        new_user = User(username=username, email=email, password=password)  
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"message": "Đăng ký thành công"}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+
+
+@user_bp.route('/login', methods=['POST'])
+def login_user():
+    try:
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        if not email or not password:
+            return jsonify({"message": "Không được bỏ trống"}), 400
+        
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not check_password_hasd(user.password, password):
+            return jsonify({"message": "Sai tài khoản hoặc mật khẩu"}), 401 
+
+        token = jwt
+
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500
+    
 
 @user_bp.route('/user', methods=["GET"])
 def get_user():
